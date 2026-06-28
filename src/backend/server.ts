40d96639e6e5fb0a20de773072672b6d46063f38
Patrick Murray <113388917+mintbandit@users.noreply.github.com>
2026-06-28 22:47:44 +0100

    Added server routes

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -5,6 +5,9 @@ import dotenv from "dotenv";
 import nunjucks from "nunjucks";
 import fastify from "fastify";
 import path from "node:path";
+import { z } from "zod";
+import { fetchLocationData } from "./location";
+import { fetchWeatherData } from "./weatherapi";
 
 dotenv.config();
 
@@ -23,7 +26,7 @@ const server = fastify({
 {
   server.register(formBody);
   server.register(staticFiles, {
-    root: path.join(__dirname,'../../dist')
+    root: path.join(__dirname,'../../dist'),
   });
 }
 
@@ -60,3 +63,50 @@ const weatherCodeToImage = (code: number): string => {
     default: return "/static/img/info.svg";
   }
 };
+
+const locationSchema = z.object({
+  location: z.string(),
+})
+
+server.get('/', async (request, reply) => {
+  const queryParams = request.query;
+  try {
+    const { location } = locationSchema.parse(queryParams);
+    const locationInfo = await fetchLocationData(HTTP_CLIENT, GEOCODE_API_URL,location);
+    const weatherInfo = await fetchWeatherData(HTTP_CLIENT, WEATHER_API_URL, locationInfo.lat, locationInfo.lon);
+    const rendered = templates.render('weather.njk', {
+      environment,
+      location: locationInfo.display_name,
+      currentDate: new Date().toDateString(),
+      weather: {
+        ...weatherInfo,
+        conditionImg: weatherCodeToImage(weatherInfo.weathercode),
+        condition: weatherInfo.condition(),
+        lowTemp: weatherInfo.lowTemp(),
+        highTemp: weatherInfo.highTemp(),
+      },
+    })
+    await reply
+      .header('Content-Type', 'text/html; charset=utf-8')
+      .send(rendered);
+  } catch(err) {
+    console.error(err);
+    const rendered = templates.render('get_started.njk',{
+      environment,
+    });
+    await reply
+      .header('Content-Type', 'text/html; charset=utf-8')
+      .send(rendered);
+  }
+});
+
+const start = async(): Promise<void> => {
+  try{
+    await server.listen({ port: 8089 });
+  } catch(e){
+    server.log.error(e);
+    process.exit(1);
+  }
+}
+
+start();
